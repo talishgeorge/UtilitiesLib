7fa439a17e6015b42944c4867071b1ca9848af95
George, Talish <talishgeorge@gmail.com>
2020-07-28 21:49:37 +0530

    added helper for weather

--- Sources/UtilitiesLib/Helper.swift
+++ Sources/UtilitiesLib/Helper.swift
@@ -5,9 +5,68 @@
 
 import Foundation
 
-/// Delay function
-/// - Parameters:
-///   - seconds: Double
-public func delay(durationInSeconds seconds: Double, completion: @escaping () -> Void) {
-    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+
+
+import Foundation
+
+public struct Helper {
+    
+    /// Delay function
+    /// - Parameters:
+    ///   - seconds: Double
+    public func delay(durationInSeconds seconds: Double, completion: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+    }
+    
+    public func timeConverter(timeStamp: Int, isDay: Bool) -> String {
+        
+        let unixTimestamp = timeStamp
+        
+        let date = Date(timeIntervalSince1970: TimeInterval(unixTimestamp))
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = isDay ? "EEEE" : "dd MMM YY, hh:mm a"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    public func showWeatherIcon(item: String) -> String {
+        return getIconName(item)
+    }
+    
+    private func getIconName(_ item: String) -> String {
+           switch item {
+           case "Ash":
+               return "cloud.hail"
+           case "Clear":
+               return "sun.max"
+           case "Clouds":
+               return "cloud.sun.fill"
+           case "Drizzle":
+               return "cloud.drizzle.fill"
+           case "Dust":
+               return "sun.dust.fill"
+           case "Fog":
+               return "cloud.fog.fill"
+           case "Haze":
+               return "sun.haze.fill"
+           case "Mist":
+               return "cloud.drizzle"
+           case "Rain":
+               return "cloud.rain.fill"
+           case "Sand":
+               return "cloud.hail"
+           case "Snow":
+               return "cloud.snow.fill"
+           case "Squall":
+               return "cloud.hail"
+           case "Thunderstorm":
+               return "cloud.bol.fill"
+           default:
+               return "clear"
+           }
+       }
 }
